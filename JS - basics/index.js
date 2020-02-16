a8bc720c19869f5182bbaaa932a6d6eee22476b4
Brian-Two <brianktoo2005@gmail.com>
2020-02-16 01:13:44 -0500

    JS - Basics

---
+++ JS - basics/index.js
@@ -0,0 +1,12 @@
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+function square(number) {
+    return number * number;
+}
+console.log(square(2));
